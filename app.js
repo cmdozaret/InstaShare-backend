db00e1ceb0d7097477c09e9b398ea8133ddc8aaa
Camilo Madariaga Dozaret <cmdozaret@gmail.com>
2024-11-06 08:29:08 -0500

    feat: allowing cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const env = require('./config/env/index');
 
@@ -7,6 +8,33 @@ const app = new express();
 
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(
+    cors({
+        maxAge: 604800000,
+        credentials: true,
+        origin(origin, callback) {
+            let corsOptions = {};
+            let error = null;
+            const allowlist = env.get('cookie:allowedOrigins');
+            if (
+                allowlist.indexOf(origin) != -1 ||
+                origin == undefined ||
+                origin == 'null'
+            ) {
+                corsOptions = {
+                    origin: true,
+                }
+            } else {
+                error = `Origin \`${origin}\` no permitido.`
+                corsOptions = {
+                    origin: false,
+                }
+            }
+            callback(error, corsOptions)
+        },
+    })
+)
 
 // loading Db configuration
 const db = require('./common/db');
